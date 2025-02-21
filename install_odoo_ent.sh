8dbe339dbed3e63c3b97e1908135853de472aede
Robby <crotti.roberto@gmail.com>
2025-02-21 12:26:01 +0100

    Update PostgreSQL version from 14 to 15 in installation script

--- install_odoo_ent.sh
+++ install_odoo_ent.sh
@@ -29,7 +29,7 @@ OE_VERSION="16.0"
 IS_ENTERPRISE="False"
 # Installs postgreSQL V14 instead of defaults (e.g V12 for Ubuntu 20/22) - this improves performance
 INSTALL_POSTGRESQL="True"
-INSTALL_POSTGRESQL_FOURTEEN="True"
+INSTALL_POSTGRESQL_FIFTEEN="True"
 # Set this to True if you want to install Nginx!
 # Set the superadmin password - if GENERATE_RANDOM_PASSWORD is set to "True" we will automatically generate a random password, otherwise we use this one
 OE_SUPERADMIN="admin"
@@ -83,12 +83,12 @@ sudo apt-get install libpq-dev git curl -y
 #--------------------------------------------------
 echo -e "\n---- Install PostgreSQL Server ----"
 if [ "$INSTALL_POSTGRESQL" = "True" ]; then
-    if [ $INSTALL_POSTGRESQL_FOURTEEN = "True" ]; then
-        echo -e "\n---- Installing postgreSQL V14 due to the user it's choise ----"
+    if [ $INSTALL_POSTGRESQL_FIFTEEN = "True" ]; then
+        echo -e "\n---- Installing postgreSQL V15 due to the user it's choise ----"
         sudo curl -fsSL https://www.postgresql.org/media/keys/ACCC4CF8.asc|sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/postgresql.gpg
         sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
         sudo apt-get update
-        sudo apt-get install postgresql-14 -y
+        sudo apt-get install postgresql-15 -y
     else
         echo -e "\n---- Installing the default postgreSQL version based on Linux version ----"
         sudo apt-get install postgresql postgresql-server-dev-all -y
